98d2ee3ae50a073c594c674a8c99256a9a97f2d8
Ivan Pryadchenko <lordpomedor@gmail.com>
2016-08-03 20:27:39 +0300

    начал добавление интерфейса на айфон

---
+++ SettingsTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsTableViewCell.swift
+//  GuitarHelper
+//
+//  Created by Ivan Pryadchenko on 03.08.16.
+//  Copyright © 2016 Ivan Pryadchenko. All rights reserved.
+//
+
+import Cocoa
+
+class SettingsTableViewCell: UITableViewCell {
+
+}
